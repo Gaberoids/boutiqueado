792397985f8704e84462c1cf73c2ff12bfcd0cf6
Gaberoids <figueiropas@hotmail.com>
2021-08-12 04:55:12 +0000

    broken test

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -153,7 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# below will tell django where static files are located
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
