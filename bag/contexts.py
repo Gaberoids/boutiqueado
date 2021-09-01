03f993cde6ef694dc562677bdb697d40d6f39a1f
Gaberoids <figueiropas@hotmail.com>
2021-09-01 14:17:52 +0000

    Finished the has_size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,15 +14,29 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+# item_data is equal quantity if no has_size
+# ... if has_size true item_data  is equal to a list dictionary of all items by size 
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):  # integer mean has_size false, item_data is quantity which is integer
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:  # settings =  boutique_ado >
         # ...settings.py
--- bag/views.py
+++ bag/views.py
@@ -15,14 +15,33 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))  # needs to have int because
     # ... comes as template from request
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     # creating a session. It stores in the HTTP until the user closes the browser
     bag = request.session.get('bag', {})  # initialized with empty dictionary
 
-# if there is a bag.keys add quantity, if not create bag_id
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+# if size is added to the item going to the card, handle things differently
+# it will be rendered in dictionary format because we can have one item but multiple sizes
+    if size:
+        # if item is already in the bag
+        if item_id in list(bag.keys()):
+            # if item with same id and same size increment the item
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                # add the item and quantity
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # if there is a bag.keys add quantity, if not create bag_id
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
 
     # put the bag variable inside of the session which is a dictionary
     request.session['bag'] = bag
