502ece78a05868a94ad09e0d9dbe69ca6e04b1a4
Gaberoids <figueiropas@hotmail.com>
2021-09-01 03:43:12 +0000

    Added has_size to Product model and selector in the template and size: to the bag page

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    
+  
     class Meta:
         verbose_name_plural = 'Categories'
 # this is to correct the plural
@@ -27,10 +27,19 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    # when changing this part above, it changes the structure of the model.
+    # ... Therefore needs to run migrations
+    # (test) python3 manage.py makemigrations --dry-run
+    # (run migration) python3 manage.py makemigrations
+    # (it shows the migrations that will be run):
+    # ... python3 manage.py migrate --plan
+
+
 
     def __str__(self):
         return self.name
