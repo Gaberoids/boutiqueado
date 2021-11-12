6eb621a391851d13363c71a17ee88c326e856c70
Gaberoids <figueiropas@hotmail.com>
2021-11-12 00:59:37 +0000

    Added profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -7,12 +7,18 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 # Create your models here.
 # Class Order = model
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    # below, set_null will allow us to keep the orders even if profile is deleted
+    # Blank= true, this way, users that are not loggin can still place orders
+    # related_name = orders, to allow dot walking like user.user_profile.orders. ...
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
