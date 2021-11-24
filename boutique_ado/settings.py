519cebc7de0475f33f7676b881601d9d63ea048c
Gaberoids <figueiropas@hotmail.com>
2021-11-24 19:11:14 +0000

    Still fixing the deployment static file problem

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -202,6 +202,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # connection to AWS.
 # keep the key secret because someone can still it and the creditcard in amazon could be charged
 if 'USE_AWS' in os.environ:
+    print("One inside use_aws---------***********-----------------**************------------")
     # Bucket Config.
     AWS_STORAGE_BUCKET_NAME = 'gaberoids-boutique-ado'
     AWS_S3_REGION_NAME = 'us-west-1'
@@ -210,12 +211,14 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files. this  goes with the code on custom_storage.py
+    print("two inside use_aws---------***********-----------------**************------------")
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
+    print("three inside use_aws---------***********-----------------**************------------")
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
